78c077c12c512f247d944a62ddda3b019c9f6814
marcosrossoni <marcosrossoni9@gmail.com>
2023-08-03 17:57:25 -0300

    Att de sidbar

--- controle-familia-frontend/src/scenes/global/SidebarGlobal.jsx
+++ controle-familia-frontend/src/scenes/global/SidebarGlobal.jsx
@@ -1,8 +1,29 @@
 import {Box, IconButton, Typography, useTheme} from "@mui/material";
-import {Sidebar, Menu, MenuItem} from "react-pro-sidebar"
+import {Menu, MenuItem, Sidebar} from "react-pro-sidebar"
 import {tokens} from "../../theme";
 import {useState} from "react";
 import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import {Link} from "react-router-dom";
+
+const Item = ({title, to, icon, selected, setSelected}) => {
+    const theme = useTheme()
+    const colors = tokens(theme.palette.mode)
+    return (
+        <MenuItem
+            active={selected === title}
+            style={{
+                color: colors.grey[100]
+            }}
+            onClick={() => setSelected(title)}
+            icon={icon}
+        >
+            <Typography>{title}</Typography>
+            <Link to={to}/>
+        </MenuItem>
+    )
+}
+
 
 const SidebarGlobal = () => {
     const theme = useTheme();
@@ -14,7 +35,7 @@ const SidebarGlobal = () => {
         <Box
             sx={{
             "& .pro-sidebar-inner": {
-                background: `${colors.primary[400]} !important`
+                background: `#f90100 !important`
             },
             "& .pro-icon-wrapper": {
                 backgroundColor: "transparent !important"
@@ -57,6 +78,47 @@ const SidebarGlobal = () => {
                         )}
                     </MenuItem>
 
+                    {!isCollapsed && (
+                        <Box mb="25px">
+                            <Box display="flex" justifyContent="center" alignItems="center">
+                                <img
+                                    alt="profile-user"
+                                    width="100px"
+                                    height="100px"
+                                    src={`../../assets/user.png`}
+                                    style={{cursor: "pointer", borderRadius: "50%"}}
+                                />
+                            </Box>
+                            <Box textAlign="center">
+                                <Typography
+                                    variant="h2"
+                                    color={colors.grey[100]}
+                                    fontWeight="bold"
+                                    sx={{m: "10px 0 0 0"}}
+                                >
+                                    Marcos Rossoni
+                                </Typography>
+                                <Typography variant="h5" color={colors.greenAccent[500]}>
+                                    Administrador
+                                </Typography>
+                            </Box>
+                        </Box>
+                    )}
+                    <Item
+                        title="Dashboard"
+                        to="/"
+                        icon={<HomeOutlinedIcon/>}
+                        selected={selected}
+                        setSelected={setSelected}
+                    />
+
+                    <Typography
+                        variant="h6"
+                        color={colors.grey[300]}
+                        sx={{m: "15px 0 5px 20px"}}
+                    >
+                        Data
+                    </Typography>
                 </Menu>
 
             </Sidebar>
