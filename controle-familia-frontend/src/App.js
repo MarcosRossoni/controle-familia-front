59e005c9bbfd43b94720e61ef5c0f2e18546fa27
marcosrossoni <marcosrossoni9@gmail.com>
2023-08-10 17:57:06 -0300

    mais alteracoes

--- controle-familia-frontend/src/App.js
+++ controle-familia-frontend/src/App.js
@@ -1,31 +1,25 @@
-import Topbar from "./scenes/global/Topbar";
-import {ColorModeContext, useMode} from "./theme";
-import {CssBaseline, ThemeProvider} from "@mui/material";
-import {Route, Routes} from "react-router-dom";
-import SidebarGlobal from "./scenes/global/SidebarGlobal";
-import Dashboard from "./scenes/dashboard/index";
-import Equipe from "./scenes/equipe/index";
-import Form from "./scenes/form";
+import './App.css';
+import Dashboard from "./dashboard/Dashboard";
 
 function App() {
-	const [theme, colorMode] = useMode()
-
-	return (<ColorModeContext.Provider value={colorMode}>
-		<ThemeProvider theme={theme}>
-			<CssBaseline />
-			<div className="app">
-				<SidebarGlobal/>
-				<main className="content">
-					<Topbar />
-					<Routes>
-						<Route exact path="/" element={<Dashboard/>}/>
-						<Route exact path="equipe" element={<Equipe/>}/>
-						<Route exact path="cadastro" element={<Form/>}/>
-					</Routes>
-				</main>
-			</div>
-		</ThemeProvider>
-	</ColorModeContext.Provider>);
+  return (
+    <div className="App">
+      <header className="App-header">
+          <Dashboard></Dashboard>
+        <p>
+         Hello
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
---
+++ controle-familia-frontend/src/dashboard/Dashboard.jsx
@@ -0,0 +1,12 @@
+import {AppBar} from "@mui/material";
+
+const Dashboard = () => {
+
+    return (
+        <AppBar position="absolute">
+            <h1>Hello</h1>
+        </AppBar>
+    )
+}
+
+export default Dashboard
