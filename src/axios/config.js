a17d462bb642de933556b9fa6adbde6f99a5c24c
marcosantonioprigolrossoni <marcosrossoni9@gmail.com>
2024-05-23 16:02:59 -0300

    ajustes

--- src/axios/config.js
+++ src/axios/config.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const pathBackend = axios.create({
+    // baseURL: "http://localhost:8080",
     baseURL: "http://localhost:8080",
     headers: {
         "Content-Type": "application/json"
