18b6f3094863b10d777a6affcaeace3b313191a6
marcosantonioprigolrossoni <marcosrossoni9@gmail.com>
2024-05-23 17:26:05 -0300

    add url de producao

--- src/axios/config.js
+++ src/axios/config.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const pathBackend = axios.create({
     // baseURL: "http://localhost:8080",
-    baseURL: "http://localhost:8080",
+    baseURL: "https://home-money-manager.up.railway.app",
     headers: {
         "Content-Type": "application/json"
     }
